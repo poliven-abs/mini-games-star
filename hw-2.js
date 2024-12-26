3579158ec93d0221ce605f493699a2eb3ff506e2
poliven-abs <ul.polina.a@gmail.com>
2024-12-26 21:57:45 +0500

    made hw-2(js)

---
+++ hw-2.js
@@ -0,0 +1,57 @@
+// task 1 //
+let a = 10;
+alert (a);
+
+a = 20;
+alert (a);
+
+// task 2 //
+let yearOfIssue = 2007;
+alert (`Год выпуска первого iPhone: ${yearOfIssue}`)
+
+// task 3 //
+let creatorName = "Брендан Эйх"
+alert (`Имя создателя языка JavaScript: ${creatorName}`)
+
+// task 4 //
+let x = 10;
+let y = 2;
+
+alert (`Задание 4: сумма ${x+y}, вычетание ${x-y}, умножение ${x*y}, деление ${x/y}`);
+
+// task 5 //
+let five = 2;
+let result = five ** 5;
+
+alert (`Задание 5: ${result}`);
+
+// task 6 //
+let a6 = 9;
+let b6 = 2;
+
+alert (`Задание 6: ${a6%b6}`);
+
+// task 7 //
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert (`Задание 7: ${num}`);
+
+// task 8 //
+let age = Number(prompt("Сколько вам лет?"));
+alert (age);
+
+// task 9 //
+const user = {
+    nameUser: "Аня",
+    age: 26,
+    isAdmin: true,
+}
+
+// task 10 //
+let custom = prompt("Ваше имя?");
+alert (`Привет, ${custom}!`);
