9447e463bf327038025bd71a64c28e5634174a66
poliven-abs <ul.polina.a@gmail.com>
2025-01-12 16:10:18 +0500

    refinement

--- hw-3.js
+++ hw-3.js
@@ -63,40 +63,24 @@ let monthNumber = Number(prompt('Введите номер месяца'));
 
 switch (monthNumber) {
     case 1:
-        console.log('Зима');
-        break;
     case 2:
+    case 12:
         console.log('Зима');
         break;
     case 3:
-        console.log('Весна');
-        break;
     case 4:
-        console.log('Весна');
-        break;
     case 5:
         console.log('Весна');
         break;
     case 6:
-        console.log('Лето');
-        break;
     case 7:
-        console.log('Лето');
-        break;
     case 8:
         console.log('Лето');
         break;
     case 9:
-        console.log('Осень');
-        break;
     case 10:
-        console.log('Осень');
-        break;
     case 11:
         console.log('Осень');
-        break;
-    case 12:
-        console.log('Зима');
         break;
     default: console.log('Ошибка ввода');
         break;
